ac9dd435284e9e1d4976ce34bcbfffb2318f7311
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2023-07-01 16:50:46 +0530

    Time: 2 ms (93.74%), Space: 61.3 MB (21.97%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 1) {
+            return 0;
+        }
+        
+        int min_so_far = prices[0];
+        int max_profit = 0;
+        for(int i =0 ; i<prices.length;i++){
+            min_so_far = Math.min(min_so_far, prices[i]);
+            int profit = prices[i]-min_so_far;
+            max_profit = Math.max(max_profit,profit);
+        }
+        return max_profit;
+    }
+}
+        
+        /* Brute force getting TLE
+        int max_profit = 0;
+        
+        for(int i=0;i<prices.length;i++) {
+            for(int j = i+1;j<prices.length;j++){
+                int profit = prices[j]-prices[i];
+                if(max_profit< profit) {
+                    max_profit= profit;
+                }
+                    
+            }
+            
+        }
+        return max_profit;
+    }
+    */
