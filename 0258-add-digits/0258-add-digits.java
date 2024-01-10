5dd2b2aed3603e9d575eeed6824f5240a24cbaaf
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-01-10 21:19:16 +0530

    Update 0258-add-digits.java

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,5 +1,7 @@
 class Solution {
     public int addDigits(int num) {
+        /* solution 1
+        
         while(num > 9) {
             int sum = 0;
             while(num > 0) {
@@ -9,6 +11,21 @@ class Solution {
             num = sum;
         }
         return num;
+        
+        */
+        
+        /* solution 2 : recurssion */
+
+        if(num <= 9) {
+            return num;
+        }
+        int sum = 0;
+        while(num > 0) {
+            sum += num%10;
+            num /= 10;
+        }
+        return addDigits(sum);   
+    }
         
     }
 }
