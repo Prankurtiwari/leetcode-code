4a10d8872ef169a36fba96d4f16fed5daf4b021f
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-01-20 18:16:52 +0530

    Time: 2 ms (96.35%), Space: 41.4 MB (74.27%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        if (arr.length == 1) {
+            return true;
+        }
+        HashMap<Integer, Integer> mp = new HashMap();
+        for(int val: arr) {
+            mp.put(val, mp.getOrDefault(val, 0) + 1);
+        }
+        HashSet<Integer> set = new HashSet<>(mp.values());
+        return mp.size() == set.size();
+    }
+}
