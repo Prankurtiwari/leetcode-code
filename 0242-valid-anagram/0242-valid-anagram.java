4af29879e52d200c3c0fbbd6d6c943d1de3a6bbb
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-01-25 22:57:12 +0530

    Time: 5 ms (49.67%), Space: 42.8 MB (76.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,48 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> mp = new HashMap<>();
+        //non-optimise
+        
+//         HashMap<Character,Integer> mp = new HashMap<>();
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+//         for(int i =0 ; i<s.length(); i++){
+//             int count = mp.getOrDefault(s.charAt(i), 0);
+//             mp.put(s.charAt(i), count+1);
+//         }
+        
+//         for(int i =0 ; i<t.length(); i++){
+//            if(mp.containsKey(t.charAt(i))) {
+//              int count = mp.get(t.charAt(i));
+//              if (count == 1) {
+//                  mp.remove(t.charAt(i));
+//              } else if (count > 1) {
+//                  mp.put(t.charAt(i), --count);
+//              }  
+//            }
+//         }
+//         return mp.size() == 0;
+//     }
+        
+        //optimise way
+        
         if (s.length() != t.length()) {
             return false;
         }
-        for(int i =0 ; i<s.length(); i++){
-            int count = mp.getOrDefault(s.charAt(i), 0);
-            mp.put(s.charAt(i), count+1);
+        
+        int[] count = new int[26];
+        for(int i =0 ; i< s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+            
         }
         
-        for(int i =0 ; i<t.length(); i++){
-           if(mp.containsKey(t.charAt(i))) {
-             int count = mp.get(t.charAt(i));
-             if (count == 1) {
-                 mp.remove(t.charAt(i));
-             } else if (count > 1) {
-                 mp.put(t.charAt(i), --count);
-             }  
-           }
+        for(int i =0; i<26; i++) {
+            if (count[i] != 0)
+                return false;
         }
-        return mp.size() == 0;
+        
+        return true;
     }
+        
 }
