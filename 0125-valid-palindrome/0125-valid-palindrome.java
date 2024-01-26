14f05124e85c542f5ab5ddb0290151016433d021
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-01-26 21:22:31 +0530

    Time: 3 ms (76.07%), Space: 42.6 MB (79.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
         if(s.equals(" ")){
             return true;
         }
@@ -26,21 +25,5 @@ class Solution {
             }
         }
         return true;
-        
     }
 }
-        
-        /*
-        s= s.toLowerCase();
-        StringBuilder r1 = new StringBuilder();
-        for(int i =0;i<s.length();i++) {
-            char c =s.charAt(i);
-            if(Character.isLetterOrDigit(c)) {
-               r1.append(c); 
-            }
-        }
-        
-        String s1 = r1.toString();
-        String s2 = r1.reverse().toString();
-        return s1.equals(s2);
-        */
