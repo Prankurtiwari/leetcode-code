01e08f3562de7543d7821de7081ad68711196bd1
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-01-27 20:41:39 +0530

    Time: 6 ms (45.30%), Space: 43.8 MB (49.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,11 +1,25 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0) {
+        
+        // non-optimised
+       /* if(x<0) {
             return false;
         }
         String str = Integer.toString(x);
         String str1 = new StringBuilder(str).reverse().toString();
 
         return str.equals(str1);
+        */
+        
+        // optimised
+        String s = String.valueOf(x); 
+        int n = s.length();
+
+        for (int i=0; i<n/2; i++) {
+            if (s.charAt(i) != s.charAt(n-i-1)){
+                return false;
+            } 
+        }
+        return true;
     }
 }
