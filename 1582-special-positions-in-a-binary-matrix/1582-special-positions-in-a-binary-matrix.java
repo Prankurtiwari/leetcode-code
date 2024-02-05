867ce9f56967789db73b56b9043f05cbd9544d3a
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-02-05 21:40:56 +0530

    Time: 2 ms (76.59%), Space: 44.7 MB (12.94%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -3,36 +3,68 @@ class Solution {
         
         
         // O(m*n + m+n) 
-        int result = 0;
-        int row = mat.length;
-        int col = mat[0].length;
+//         int result = 0;
+//         int row = mat.length;
+//         int col = mat[0].length;
         
-        for(int i =0; i<row; i++) {
-            for(int j = 0; j<col; j++) {
-                if (mat[i][j] == 0) {
-                    continue;
-                }
-                boolean correct = true;
+//         for(int i =0; i<row; i++) {
+//             for(int j = 0; j<col; j++) {
+//                 if (mat[i][j] == 0) {
+//                     continue;
+//                 }
+//                 boolean correct = true;
                 
-               for(int a =0; a<row; a++){
-                    if(a != i && mat[a][j] == 1) {
-                        correct =false;
-                        break;
-                    }
-                }
+//                for(int a =0; a<row; a++){
+//                     if(a != i && mat[a][j] == 1) {
+//                         correct =false;
+//                         break;
+//                     }
+//                 }
                 
-                for(int a =0; a<col; a++){
-                    if(a != j && mat[i][a] == 1) {
-                        correct =false;
-                        break;
-                    }
-                }
-                if (correct) {
-                    result++;
+//                 for(int a =0; a<col; a++){
+//                     if(a != j && mat[i][a] == 1) {
+//                         correct =false;
+//                         break;
+//                     }
+//                 }
+//                 if (correct) {
+//                     result++;
+//                 }
+//             }
+//         }
+        
+//         return result;
+        
+        
+        
+        
+        // optimize-solution
+        int result = 0;
+        int rows = mat.length;
+        int cols = mat[0].length;
+        
+        
+        int[] row = new int[rows];
+        int[] col = new int[cols];
+        
+        for(int i =0; i<rows; i++) {
+            for(int j = 0; j<cols; j++) {
+                if (mat[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
                 }
             }
         }
         
-        return result;
+        
+        for(int i=0; i<rows; i++) {
+            for(int j = 0; j<cols; j++) {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j]  == 1) {
+                    result++;
+                }
+            }
+        }
+    
+        return result;  
     }
 }
