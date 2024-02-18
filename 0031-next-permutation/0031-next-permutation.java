06951f3b6d47595367da5f5b7dfb4ad6b75c4220
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-02-18 16:58:31 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (59.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,5 +1,21 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        
+        
+        /*
+        Learn yard lecture  on 17 Feb.
+        1.Start from end and move the ith pointer till it is non-ascedning.
+        2.Now when i pointer is at specific index. Reiterate from last to ith with j pointer till nums[i]>= nums[j] i.e find first number greater than nums[i].
+        3. Swap i and j.
+        4. reverse i+1 till end.
+        
+        */
+        
+        
+        
+        
+        
+        
         int n = nums.length;
         int i = n-2;
         
