433729c246e89d6bf722a0de9fe3067e85efafb3
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-02-29 13:24:01 +0530

    Time: 5 ms (43.33%), Space: 57.6 MB (51.78%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,5 +1,27 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
+        //Discussed in lecture 33 on Feb 28 2024        
+        
+
+        /*
+        
+        for every a[i] the logic is 
+        res[i] = left + right;(total n-1 terms)
+        left part has i terms
+                (a[i] - a[0]) + (a[i] - a[1])......+(a[i] - a[i-1])
+               = (i*a[i]) - (a[0] + a[1]....+a[i-1])
+               = (i*a[i]) - prefix_sum_of_i-1;
+               
+         right part has n-1 terms
+                (a[i+1] - a[i]) + (a[i+1] - a[i]) .....+(a[n-1] -a[i])
+                = (a[i+1] + a[i+2] .....a[n-1]) - (a[i] * (n-1-i))
+                = sufix_sum_of_i+1 - (a[i] * (n-1-i))
+        
+        
+        
+        */
+
+
         int size = nums.length;
         int prefixSum[] = new int[size];
         int sufixSum[] = new int[size];
