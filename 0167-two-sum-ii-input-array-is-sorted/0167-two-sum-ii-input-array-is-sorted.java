7bc0428923d827a1215cce8df58185045f8601bd
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-03-06 15:40:39 +0530

    Time: 3 ms (28.44%), Space: 47.1 MB (53.69%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -4,14 +4,23 @@ class Solution {
         int start = 0;
         int end = nums.length-1;
         while(start <= end) {
+            int mid = (start+end)/2;
             if (nums[start] + nums[end] == target) {
                 res[0] = start+1;
                 res[1] = end+1;
                 break;
             } else if (nums[start] + nums[end] > target) {
-                end--;
+                if (nums[start] + nums[mid] > target) {
+                    end = mid -1;
+                } else {
+                    end--;
+                }
             } else {
-                start++;
+                if (nums[start] + nums[mid] < target) {
+                    start = start +1;
+                } else {
+                   start++;
+                } 
             }
         }
         
