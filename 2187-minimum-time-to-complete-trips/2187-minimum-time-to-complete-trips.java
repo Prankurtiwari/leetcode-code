8f6b17c66b071fea55d8238f0da171fbce8b4375
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-03-09 20:45:38 +0530

    Time: 63 ms (64.94%), Space: 57.6 MB (76.54%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -1,26 +1,48 @@
 class Solution {
+    /*
+
+Question - Minimum Time to Complete Trips (Leetcode)
+Link  - https://leetcode.com/problems/minimum-time-to-complete-trips/description/
+
+
+The minimum time required can be 1; less than 1 is not possible.
+
+The maximum time required can be (the minimum element of the given time array * total trips).
+
+We use binary search to optimally choose the minimum time required to complete the total trips.
+
+Here, we assume an answer and attempt to verify whether our assumed answer is possible or not.
+
+If the number of trips completed by the 'mid' time is greater than or equal to the total trips, then our answer can be 'mid' time. However, it's possible that a time less than 'mid' can also be sufficient, so we reduce our end to decrease the 'mid'.
+
+If the number of trips completed by the 'mid' time is less than the total trips, then our answer cannot be 'mid' time. However, it's possible that a time greater than 'mid' can be sufficient, so we increase our start to increase 'mid'.
+
+
+*/
+    
+    
     public long minimumTime(int[] time, int totalTrips) {
-       int n = time.length;
-        Arrays.sort(time);
-        long low = (long) time[0], high = (long) time[n - 1] * totalTrips;
-        while (low < high) {
-            long mid = low + (high - low) / 2;
-            if (check(time, mid, totalTrips)){
-                high = mid;
-            }else
-                low = mid + 1;
+        long st = 0, ed = (time[0] + 0L) * totalTrips;
+        long ans = 0;
+        while (st <= ed) {
+            long mid = (st + ed) / 2;
+
+            if (isPossible(mid, totalTrips, time)) {
+                ans = mid;
+                ed = mid - 1;
+            } else {
+                st = mid + 1;
+            }
         }
-        return low;
+        return ans;
     }
     
-    private boolean check(int[] time, long mid, int totalTrips) {
-        long trips = 0;
-        for (int x : time) {
-            trips += (mid / x);
-            if (trips >= totalTrips)
-                return true;
+   public boolean isPossible(long mid, int totalTrips, int[] time) {
+        long ct = 0;
+        for (int i = 0; i < time.length; i++) {
+            ct += mid / time[i];
         }
-        return false;
+        return ct >= totalTrips;
     }
 
 }
