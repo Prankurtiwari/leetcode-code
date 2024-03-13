0c401c695dd709b744949c43aae99b62ed7ec247
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-03-13 21:29:30 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (15.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,28 +10,44 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-         ListNode temp_node = new ListNode(0);
-        ListNode cur_node = temp_node;
+//          ListNode temp_node = new ListNode(0);
+//         ListNode cur_node = temp_node;
         
-        while( list1 != null && list2 != null) {
-            if(list1.val < list2.val) {
-                cur_node.next = list1;
-                list1= list1.next;
-            }
-             else {
-                cur_node.next = list2;
-                list2= list2.next;
-            }
-            cur_node = cur_node.next != null ? cur_node.next : cur_node;
+//         while( list1 != null && list2 != null) {
+//             if(list1.val < list2.val) {
+//                 cur_node.next = list1;
+//                 list1= list1.next;
+//             }
+//              else {
+//                 cur_node.next = list2;
+//                 list2= list2.next;
+//             }
+//             cur_node = cur_node.next != null ? cur_node.next : cur_node;
+//         }
+//         if (list1 != null) {
+//             cur_node.next = list1;
+//             // list1 = list1.next;
+//         }
+//         if (list2 != null) {
+//             cur_node.next = list2;
+//             // list2 = list2.next;
+//         }
+//         return temp_node.next;
+        
+        
+        
+        // Recurssion
+
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+        
+        if (list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-        if (list1 != null) {
-            cur_node.next = list1;
-            // list1 = list1.next;
-        }
-        if (list2 != null) {
-            cur_node.next = list2;
-            // list2 = list2.next;
-        }
-        return temp_node.next;
+        
     }
 }
