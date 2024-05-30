914624bdde6dce6991936512a263c80a325030aa
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2024-05-30 18:29:16 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (34.46%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -27,28 +27,56 @@ class Solution {
     
     
     // using 2- stack
-    public void postorder(TreeNode root, List<Integer> res) {
-        Stack<TreeNode> st1 = new Stack<TreeNode>();
-        Stack<TreeNode> st2 = new Stack<TreeNode>();
-        if (root == null ) {
+//     public void postorder(TreeNode root, List<Integer> res) {
+//         Stack<TreeNode> st1 = new Stack<TreeNode>();
+//         Stack<TreeNode> st2 = new Stack<TreeNode>();
+//         if (root == null ) {
+//             return;
+//         }
+//         st1.push(root);
+//         while(!st1.isEmpty()) {
+//             TreeNode node= st1.pop();
+//             if (node.left != null) {
+//                 st1.push(node.left);
+//             }
+//             if (node.right != null) {
+//                 st1.push(node.right);
+//             }
+//             st2.push(node);
+//         }
+        
+//         while(!st2.isEmpty()) {
+//             res.add(st2.pop().val);
+//         }
+//     }
+    
+    // using 1 stack (imp)
+     public void postorder(TreeNode root, List<Integer> res) {
+         Stack<TreeNode> st = new Stack<TreeNode>();
+         if (root == null ) {
             return;
         }
-        st1.push(root);
-        while(!st1.isEmpty()) {
-            TreeNode node= st1.pop();
-            if (node.left != null) {
-                st1.push(node.left);
-            }
-            if (node.right != null) {
-                st1.push(node.right);
-            }
-            st2.push(node);
+        TreeNode cur =root;
+        while(cur != null || !st.isEmpty()) {
+             if (cur != null) {
+                 st.push(cur);
+                 cur = cur.left;
+             } else {
+                 TreeNode temp = st.peek().right;
+                 if (temp == null) {
+                     temp = st.pop();
+                     res.add(temp.val);
+                     while(!st.isEmpty() && temp == st.peek().right) {
+                         temp = st.pop();
+                         res.add(temp.val);
+                     }
+                 } else {
+                     cur = temp;
+                 }
+             }
         }
-        
-        while(!st2.isEmpty()) {
-            res.add(st2.pop().val);
-        }
-    }
+         
+     }
     
     
     public List<Integer> postorderTraversal(TreeNode root) {
