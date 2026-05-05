0ebbbe1ee33d35b0058fac1061238d913efcbac4
Prankur Tiwari <43743993+Prankurtiwari@users.noreply.github.com>
2026-05-06 00:32:54 +0530

    Time: 0 ms (100%), Space: 42.5 MB (95.48%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isRobotBounded(String ins) {
+        int x =0;
+        int y =0;
+        int dx =0;
+        int dy =1;
+
+        for(char ch : ins.toCharArray()) {
+            if(ch == 'G') {
+                x+=dx;
+                y+=dy;
+            } else if (ch =='L') {
+                int temp = dx;
+                dx = dy;
+                dy = temp;
+                dy = (-1) * dy;
+            } else if (ch == 'R') {
+                int temp = dx;
+                dx = dy;
+                dy = temp;
+                dx = (-1) * dx;
+            }
+        }
+
+        return (x == 0 && y ==0) || (dx != 0 || dy != 1);
+    }
+}
